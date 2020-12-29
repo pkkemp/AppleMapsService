30f5e1a90d946d8a93ea765804e74f291bdff0ff
Preston Kemp <preston@prestonkemp.com>
2020-12-28 22:02:40 -0600

    changes for Google Cloud Function capability

--- main.go
+++ main.go
@@ -5,13 +5,12 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"io/ioutil"
-	"os"
 	"log"
+	"net/http"
 	"strings"
 	"time"
-	"github.com/dgrijalva/jwt-go"
-	"net/http"
 )
 
 // Create the JWT key used to create the signature
@@ -32,62 +31,7 @@ jwt.StandardClaims
 
 //Generate MapKitJS Token
 func GenerateMapsToken(w http.ResponseWriter, r *http.Request) {
-	sites, ok := r.URL.Query()["site"]
-	if !ok || len(sites[0]) < 1 {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("Url Param 'Site' is missing")
-		return
-	}
-
-	// Query()["key"] will return an array of items,
-	// we only want the single item.
-	site := sites[0]
-
-	//validate if this is a URL we should provide a signed token for
-	URLComponents := strings.Split(site, ".")
-	numComponents := len(URLComponents)
-	if(URLComponents[numComponents-1] != "org" ||
-		(URLComponents[numComponents-2] != "freemomhugs" &&
-			URLComponents[numComponents-2] != "https://freemomhugs" )) {
-		w.WriteHeader(http.StatusUnauthorized)
-		log.Println("Invalid Site")
-		return
-	}
-
-
-	var claims Claims
-	claims.IssuedAt = time.Now().Unix()
-	claims.Issuer = "6G53WHVXA8"
-
-	claims.Origin = site
-	//set the token to expire 30 minutes from now
-	claims.ExpiresAt = time.Now().Add(30 * time.Minute).Unix()
-
-	// Declare the token with the algorithm used for signing, and the claims
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-	token.Header["kid"] = mapsKeyID
-	// Create the JWT string
-	tokenString, err := token.SignedString(mapsKey)
-	if err != nil {
-		// If there is an error in creating the JWT return an internal server error
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Finally, we set the client cookie for "token" as the JWT we just generated
-	// we also set an expiry time which is the same as the token itself
-	http.SetCookie(w, &http.Cookie{
-		Name:    "token",
-		Value:   tokenString,
-		Expires: time.Now().Add(30 * time.Minute),
-	})
-	w.Header().Set("Access-Control-Allow-Origin", site)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(tokenString))
-}
-
-func main() {
-	p8bytes, err := ioutil.ReadFile("AuthKey_F8YD4N54KD.p8")
+	p8bytes, err := ioutil.ReadFile("AuthKey_649ZMYA975.p8")
 	if err != nil {
 		log.Println(err)
 		return
@@ -119,18 +63,56 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	//register the maps jwt function
-	http.HandleFunc("/maps/jwt", GenerateMapsToken)
-	http.Handle("/", http.FileServer(http.Dir("./debug")))
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+	sites, ok := r.URL.Query()["site"]
+	if !ok || len(sites[0]) < 1 {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Url Param 'Site' is missing")
+		return
 	}
 
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
+	// Query()["key"] will return an array of items,
+	// we only want the single item.
+	site := sites[0]
+
+	//validate if this is a URL we should provide a signed token for
+	URLComponents := strings.Split(site, ".")
+	numComponents := len(URLComponents)
+	if(URLComponents[numComponents-1] != "org" ||
+		(URLComponents[numComponents-2] != "freemomhugs" &&
+			URLComponents[numComponents-2] != "https://freemomhugs" )) {
+		w.WriteHeader(http.StatusUnauthorized)
+		log.Println("Invalid Site")
+		return
 	}
+
+
+	var claims Claims
+	claims.IssuedAt = time.Now().Unix()
+	claims.Issuer = "649ZMYA975"
+
+	claims.Origin = site
+	//set the token to expire 30 minutes from now
+	claims.ExpiresAt = time.Now().Add(30 * time.Minute).Unix()
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token.Header["kid"] = mapsKeyID
+	// Create the JWT string
+	tokenString, err := token.SignedString(mapsKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: time.Now().Add(30 * time.Minute),
+	})
+	w.Header().Set("Access-Control-Allow-Origin", site)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(tokenString))
 }
