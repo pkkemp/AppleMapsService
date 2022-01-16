562290f6f2f611c9b04d9bb9b0c572c24bd8ca29
Preston <preston@prestonkemp.com>
2022-01-16 12:40:39 -0600

    update for cloud run key

--- main.go
+++ main.go
@@ -5,29 +5,28 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	jwt "github.com/dgrijalva/jwt-go"
 	"io/ioutil"
-	"os"
 	"log"
+	"net/http"
+	"os"
 	"strings"
 	"time"
-	"github.com/dgrijalva/jwt-go"
-	"net/http"
 )
 
 // Create the JWT key used to create the signature
 var rawJwtKey []byte
 var mapsKey *ecdsa.PrivateKey
 
-var mapsKeyID =  "F8YD4N54KD"
+var mapsKeyID = "ZA2Y7ZR45P"
 
 //JWT Claims required by MapKitJS
 type Claims struct {
-Issuer string `json:"iss"`
-IssuedAt int64 `json:"iat"`
-KeyID string `json:"kid"`
-Origin string `json:"origin"`
-jwt.StandardClaims
-
+	Issuer   string `json:"iss"`
+	IssuedAt int64  `json:"iat"`
+	KeyID    string `json:"kid"`
+	Origin   string `json:"origin"`
+	jwt.StandardClaims
 }
 
 //Generate MapKitJS Token
@@ -46,14 +45,13 @@ func GenerateMapsToken(w http.ResponseWriter, r *http.Request) {
 	//validate if this is a URL we should provide a signed token for
 	URLComponents := strings.Split(site, ".")
 	numComponents := len(URLComponents)
-	if(URLComponents[numComponents-1] != "org" ||
+	if URLComponents[numComponents-1] != "org" ||
 		(URLComponents[numComponents-2] != "freemomhugs" &&
-			URLComponents[numComponents-2] != "https://freemomhugs" )) {
+			URLComponents[numComponents-2] != "https://freemomhugs") {
 		w.WriteHeader(http.StatusUnauthorized)
 		log.Println("Invalid Site")
 		return
 	}
-
 
 	var claims Claims
 	claims.IssuedAt = time.Now().Unix()
@@ -87,7 +85,7 @@ func GenerateMapsToken(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	p8bytes, err := ioutil.ReadFile("AuthKey_F8YD4N54KD.p8")
+	p8bytes, err := ioutil.ReadFile("AuthKey_ZA2Y7ZR45P.p8")
 	if err != nil {
 		log.Println(err)
 		return
@@ -126,7 +124,6 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		log.Printf("Defaulting to port %s", port)
 	}
 
 	log.Printf("Listening on port %s", port)
